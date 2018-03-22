7dbd07cc83808a7ff1f8c57f45ea86eb45130e09
Hong Zhou <zhsnnu@gmail.com>
2018-03-22 15:24:28 -0500

    add findBy methods in the repository for REST api

--- src/main/java/com/hongzhou/ec/repos/TourPackageRepository.java
+++ src/main/java/com/hongzhou/ec/repos/TourPackageRepository.java
@@ -1,10 +1,11 @@
 package com.hongzhou.ec.repos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.hongzhou.ec.domain.TourPackage;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
-	TourPackage findByName(String name);
+	TourPackage findByName(@Param("name") String name);
 }
