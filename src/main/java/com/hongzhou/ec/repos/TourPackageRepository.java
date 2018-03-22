0bef341a4408dbd2e6c7c57cd13ffc38e158baa8
Hong Zhou <zhsnnu@gmail.com>
2018-03-22 15:50:57 -0500

    control API exposure by @RestResource annotation

--- src/main/java/com/hongzhou/ec/repos/TourPackageRepository.java
+++ src/main/java/com/hongzhou/ec/repos/TourPackageRepository.java
@@ -2,10 +2,37 @@ package com.hongzhou.ec.repos;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.hongzhou.ec.domain.TourPackage;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path="packages") // rename endpoint of rest api
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
 	TourPackage findByName(@Param("name") String name);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Iterable<? extends TourPackage> arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(String arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(TourPackage arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll();
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> Iterable<S> save(Iterable<S> arg0);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> S save(S arg0);			
 }
--- src/main/java/com/hongzhou/ec/repos/TourRepository.java
+++ src/main/java/com/hongzhou/ec/repos/TourRepository.java
@@ -1,12 +1,40 @@
 package com.hongzhou.ec.repos;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.hongzhou.ec.domain.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-	List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends /*CrudRepository<Tour, Integer>*/ PagingAndSortingRepository<Tour, Integer> {
+	//List<Tour> findByTourPackageCode(@Param("code") String code);
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Integer arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Iterable<? extends Tour> arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Tour arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll();
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> Iterable<S> save(Iterable<S> arg0);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> S save(S arg0);
+	
+	
 }
